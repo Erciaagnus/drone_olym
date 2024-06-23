601714b2c56deb66fb89a7b39c2d862900e07de4
JEON <devote010409@gmail.com>
2024-06-23 17:04:50 +0900

    first commit

---
+++ scripts/pesudo_sensor_node.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+# 가상의 센서값을 출력 : 목표 위치
+# 컨트롤러가 이 값을 받아서 PID 계산. 
+# pesudo_sensor_node_py 노드
---
+++ scripts/xyz_controller_node.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python
+# 센서 등으로 받아서 PID 산출 -> 속도 제어 명령
+# 노드이름 : xyz_controller_node_py
