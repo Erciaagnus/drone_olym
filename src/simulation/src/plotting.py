cbf46bad6d8bda6e5891d69929a100bff7e5dfd5
henricus <henricus0973@korea.ac.kr>
2024-08-30 03:24:13 +0900

    Added Target Picture to the plotting

--- src/simulation/src/plotting.py
+++ src/simulation/src/plotting.py
@@ -9,6 +9,7 @@ relative_path = os.path.join(current_dir, '../', 'traj', 'uav_trajectory_m_3_see
 with open(relative_path, 'rb') as file:
     uav_trajectory_data = pickle.load(file)
 
+target_position = [[800, 1430], [-1180, 700], [1200, 1300], [-1100, -1200], [800, -1120]]
 # 각 UAV의 좌표 추출
 uav0_coords = [(data[0], data[1]) for data in uav_trajectory_data[0]]
 uav1_coords = [(data[0], data[1]) for data in uav_trajectory_data[1]]
@@ -18,12 +19,18 @@ uav2_coords = [(data[0], data[1]) for data in uav_trajectory_data[2]]
 uav0_x, uav0_y = zip(*uav0_coords)
 uav1_x, uav1_y = zip(*uav1_coords)
 uav2_x, uav2_y = zip(*uav2_coords)
+target_x, target_y = zip(*target_position)
 
 # 그래프 그리기
 plt.figure(figsize=(10, 8))
-plt.plot(uav0_x, uav0_y, label='UAV 0', marker='o')
-plt.plot(uav1_x, uav1_y, label='UAV 1', marker='x')
-plt.plot(uav2_x, uav2_y, label='UAV 2', marker='s')
+
+# UAV 경로를 먼저 그리기
+plt.plot(uav0_x, uav0_y, label='UAV 0', marker='o', zorder=1)
+plt.plot(uav1_x, uav1_y, label='UAV 1', marker='x', zorder=1)
+plt.plot(uav2_x, uav2_y, label='UAV 2', marker='s', zorder=1)
+
+# 타겟 마커를 나중에 그리기 (가장 위에 그려짐)
+plt.scatter(target_x, target_y, color='red', marker='^', s=100, label='Targets', zorder=2)
 
 plt.title('UAV Trajectories')
 plt.xlabel('X Coordinate')
